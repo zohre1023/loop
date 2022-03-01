b9744c30e0566c4ae8f49b7604997ba6283e678a
zohre1023 <zohre1023@gmail.com>
2022-03-01 15:21:02 +0330

    start news overlay

--- src/pages/homepage/News/NewsModal/articles.jsx
+++ src/pages/homepage/News/NewsModal/articles.jsx
@@ -1,22 +1,38 @@
 import React from "react";
-import { Carousel } from "antd";
 import H3 from "components/HeadTags/H3/index";
 import { ARTICLE_TITLE } from "helper/newsConstants";
 import { articleData } from "./articleData";
 import ArticleItem from "./articleItem";
+import { Carousel } from "@trendyol-js/react-carousel";
+import Image from "components/Image/index";
+import Arrow from "assets/images/arrowBlack.png";
+
 const Articles = () => {
   return (
     <div className="articles">
-      <div>
+      <div className="title">
         <H3 className="color-red">{ARTICLE_TITLE}</H3>
       </div>
-      <div className="">
-        <Carousel swipeToSlide>
-          {articleData?.map((article) => (
-            <ArticleItem key={article?.id} article={article} />
-          ))}
-        </Carousel>
-      </div>
+
+      <Carousel
+        show={4}
+        slide={3}
+        swiping={true}
+        rightArrow={
+          <div className="right-arrow">
+            <Image src={Arrow} />
+          </div>
+        }
+        leftArrow={
+          <div className="left-arrow">
+            <Image src={Arrow} />
+          </div>
+        }
+      >
+        {articleData?.map((article) => (
+          <ArticleItem key={article?.id} article={article} />
+        ))}
+      </Carousel>
     </div>
   );
 };
